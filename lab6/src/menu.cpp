06f1cef8fccae3a05b3ab77c27aa866eb4d6167b
Pavel <shmyginp05@gmail.com>
2025-11-02 19:21:08 +0300

    fix

--- lab6/src/menu.cpp
+++ lab6/src/menu.cpp
@@ -7,25 +7,27 @@ using namespace std;
 
 void showMenu() {
     string date;
+    bool continueInput = true;
 
-    while (true) {
+    while (continueInput) {
         cout << "Введите дату в формате дд.мм.гг (или 0 для выхода): ";
         cin >> date;
 
         if (date == "0") {
             cout << "Выход из программы..." << endl;
-            break;
+            continueInput = false;
         }
-
-        try {
-            DateValidator::checkDate(date);
-            cout << " Дата введена корректно!" << endl;
-            cout << "Программа завершена." << endl;
-            break;
-        }
-        catch (const DateFormatException& e) {
-            cout << " " << e.what() << endl << endl;
-            cout << "Попробуйте снова." << endl << endl;
+        else {
+            try {
+                DateValidator::checkDate(date);
+                cout << " Дата введена корректно!" << endl;
+                cout << "Программа завершена." << endl;
+                continueInput = false;
+            }
+            catch (const DateFormatException& e) {
+                cout << " " << e.what() << endl << endl;
+                cout << "Попробуйте снова." << endl << endl;
+            }
         }
     }
 }
